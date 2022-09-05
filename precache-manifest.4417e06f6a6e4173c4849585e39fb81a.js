c9bede81fa06ee626fd5f8a2980443ae7cfa0f00
skooldyprogram <112844120+skooldyprogram@users.noreply.github.com>
2022-09-05 00:50:48 -0500

    Add files via upload

---
+++ precache-manifest.4417e06f6a6e4173c4849585e39fb81a.js
@@ -0,0 +1,546 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a115380f9a0d37fc41c4419e119647b",
+    "url": "/Skooldy/index.html"
+  },
+  {
+    "revision": "5a09188271bbc9a70f95",
+    "url": "/Skooldy/static/css/2.5a7cb3a2.chunk.css"
+  },
+  {
+    "revision": "984e03390a4624723328",
+    "url": "/Skooldy/static/css/main.8b1544a8.chunk.css"
+  },
+  {
+    "revision": "5a09188271bbc9a70f95",
+    "url": "/Skooldy/static/js/2.fdc81c2d.chunk.js"
+  },
+  {
+    "revision": "8aebae742d39d97d1a7e54d2fe0e7a71",
+    "url": "/Skooldy/static/js/2.fdc81c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "984e03390a4624723328",
+    "url": "/Skooldy/static/js/main.78d787e6.chunk.js"
+  },
+  {
+    "revision": "67123737ee35010fe26f",
+    "url": "/Skooldy/static/js/runtime-main.c28a7828.js"
+  },
+  {
+    "revision": "43d99bb906019397b9591559fb3134ef",
+    "url": "/Skooldy/static/media/AlienGame.43d99bb9.mp3"
+  },
+  {
+    "revision": "f0988c209eeb180ea9b28017272dc30d",
+    "url": "/Skooldy/static/media/AlienGame1.f0988c20.gif"
+  },
+  {
+    "revision": "1b2a1150352a277406c6600a63d412f5",
+    "url": "/Skooldy/static/media/AlienGame2.1b2a1150.gif"
+  },
+  {
+    "revision": "acc571f0e2faf46af7069d51844d010f",
+    "url": "/Skooldy/static/media/AlienGame3.acc571f0.gif"
+  },
+  {
+    "revision": "ac483cf1a33d049ed70fbba5aa4fdf16",
+    "url": "/Skooldy/static/media/AlienGame4.ac483cf1.gif"
+  },
+  {
+    "revision": "3733b27482d4239d70851dddea65dc2c",
+    "url": "/Skooldy/static/media/Alumno.3733b274.svg"
+  },
+  {
+    "revision": "81342ef5c12cca8751ef5ca7e24635a7",
+    "url": "/Skooldy/static/media/BORRAR.81342ef5.jpg"
+  },
+  {
+    "revision": "f6bb2d425a1d6492489d946ccc686d9c",
+    "url": "/Skooldy/static/media/Back.f6bb2d42.svg"
+  },
+  {
+    "revision": "3dd00b44221d4d3bacea131720564c9c",
+    "url": "/Skooldy/static/media/Caja.3dd00b44.png"
+  },
+  {
+    "revision": "35b1c69da365471a8b52200e63a81c49",
+    "url": "/Skooldy/static/media/Caja_movil.35b1c69d.svg"
+  },
+  {
+    "revision": "1be40c8488583c9891407a96aaba17ee",
+    "url": "/Skooldy/static/media/Caja_web.1be40c84.svg"
+  },
+  {
+    "revision": "6c3e15c76e8916b387ec21ab153659be",
+    "url": "/Skooldy/static/media/Correcta.6c3e15c7.mp3"
+  },
+  {
+    "revision": "150391f85d7ac69ba9bd085e4abad161",
+    "url": "/Skooldy/static/media/Derecha.150391f8.svg"
+  },
+  {
+    "revision": "c747a3faa546819a903aadc269397880",
+    "url": "/Skooldy/static/media/Estudios.c747a3fa.svg"
+  },
+  {
+    "revision": "9cf36af100676ac660753af53b22f2cf",
+    "url": "/Skooldy/static/media/Explotar1.9cf36af1.png"
+  },
+  {
+    "revision": "0522e922200396638b47138eb848b16a",
+    "url": "/Skooldy/static/media/Explotar10.0522e922.png"
+  },
+  {
+    "revision": "fb0a65c295991e3003849633166eac0f",
+    "url": "/Skooldy/static/media/Explotar11.fb0a65c2.png"
+  },
+  {
+    "revision": "f50e136f3c714041fe1beca88f5f92e1",
+    "url": "/Skooldy/static/media/Explotar12.f50e136f.png"
+  },
+  {
+    "revision": "c8e11a98bab99f6ebca3f531ae032bf1",
+    "url": "/Skooldy/static/media/Explotar13.c8e11a98.png"
+  },
+  {
+    "revision": "4963933cb4f9afd3014ecc3b1b95d69f",
+    "url": "/Skooldy/static/media/Explotar14.4963933c.png"
+  },
+  {
+    "revision": "8211ed72bc98fcfc087410dcbe954dec",
+    "url": "/Skooldy/static/media/Explotar15.8211ed72.png"
+  },
+  {
+    "revision": "0b6d6580811020e7a4466b0496d73fee",
+    "url": "/Skooldy/static/media/Explotar16.0b6d6580.png"
+  },
+  {
+    "revision": "2d2cff72a50de881a30aac0cfce68414",
+    "url": "/Skooldy/static/media/Explotar2.2d2cff72.png"
+  },
+  {
+    "revision": "847213658d32b6eb7e3e9f1c2f7d9b3e",
+    "url": "/Skooldy/static/media/Explotar3.84721365.png"
+  },
+  {
+    "revision": "4cb72943e6288646a2479250f3dd7503",
+    "url": "/Skooldy/static/media/Explotar4.4cb72943.png"
+  },
+  {
+    "revision": "e1428a0b14259f81359ab6d6f56b6950",
+    "url": "/Skooldy/static/media/Explotar5.e1428a0b.png"
+  },
+  {
+    "revision": "92b9c302ca1c24d73977a8d96984e6fe",
+    "url": "/Skooldy/static/media/Explotar6.92b9c302.png"
+  },
+  {
+    "revision": "b4c6bb3a10ff54aa665645a8706d9cb1",
+    "url": "/Skooldy/static/media/Explotar7.b4c6bb3a.png"
+  },
+  {
+    "revision": "af64200b9ef45259e36a33ee284a76da",
+    "url": "/Skooldy/static/media/Explotar8.af64200b.png"
+  },
+  {
+    "revision": "ff4d396260569fdd0ad258023000d24a",
+    "url": "/Skooldy/static/media/Explotar9.ff4d3962.png"
+  },
+  {
+    "revision": "742dd9d1a872e0b588950f08eccfd10d",
+    "url": "/Skooldy/static/media/Fondo.742dd9d1.png"
+  },
+  {
+    "revision": "db6651e6b41fa87ca7bf5e63b533b0b5",
+    "url": "/Skooldy/static/media/FondoConjuntos.db6651e6.png"
+  },
+  {
+    "revision": "9a79911dbec4e7ca74ce99f85fb08d89",
+    "url": "/Skooldy/static/media/FondoN.9a79911d.png"
+  },
+  {
+    "revision": "07b6cef9e4b14fb5f156205b84c3987f",
+    "url": "/Skooldy/static/media/IconNewAct.07b6cef9.svg"
+  },
+  {
+    "revision": "c2050afa2713768101c8a30f7bf95b66",
+    "url": "/Skooldy/static/media/Incorrecta.c2050afa.mp3"
+  },
+  {
+    "revision": "2e63f86557c6e927d47df54435159fb2",
+    "url": "/Skooldy/static/media/Izq.2e63f865.svg"
+  },
+  {
+    "revision": "406d0eb8854c986012c7e2b29d510979",
+    "url": "/Skooldy/static/media/Messages.406d0eb8.svg"
+  },
+  {
+    "revision": "3ce06f19f70cfaa5c6d0a2f31ade01e4",
+    "url": "/Skooldy/static/media/MusicNumeros.3ce06f19.mp3"
+  },
+  {
+    "revision": "9dbf3bfb21394bc5120dffa799f30f25",
+    "url": "/Skooldy/static/media/Nacion.9dbf3bfb.svg"
+  },
+  {
+    "revision": "88a840c6201055be3138a8c0b3b0298f",
+    "url": "/Skooldy/static/media/NumerosGame1.88a840c6.gif"
+  },
+  {
+    "revision": "ec5d5f2d3d6091abfe06e457c2d250d9",
+    "url": "/Skooldy/static/media/NumerosGame2.ec5d5f2d.gif"
+  },
+  {
+    "revision": "4b924d5dfd10c6ef30eb08179811256c",
+    "url": "/Skooldy/static/media/NumerosGame3.4b924d5d.gif"
+  },
+  {
+    "revision": "6dc565cdd6de31f30f432204a5b94527",
+    "url": "/Skooldy/static/media/NumerosGame4.6dc565cd.gif"
+  },
+  {
+    "revision": "cec1eb8a97f9753027fbbd22ab9f69d6",
+    "url": "/Skooldy/static/media/Plataforma_movil.cec1eb8a.svg"
+  },
+  {
+    "revision": "cbc3a967b243ad4fc9d8e05f6bf739d8",
+    "url": "/Skooldy/static/media/PlataformasGame1.cbc3a967.gif"
+  },
+  {
+    "revision": "28cf5f9ea5e4372e48f338fa333cd3d8",
+    "url": "/Skooldy/static/media/PlataformasGame2.28cf5f9e.gif"
+  },
+  {
+    "revision": "356d930b1860a1e45e3bdd50369d3f06",
+    "url": "/Skooldy/static/media/PlataformasGame3.356d930b.gif"
+  },
+  {
+    "revision": "8bbf2cb190ec9169c2bb79b3b0971d28",
+    "url": "/Skooldy/static/media/PlataformasGame4.8bbf2cb1.gif"
+  },
+  {
+    "revision": "3e876b57d56fa2030f9bbfcdafa69297",
+    "url": "/Skooldy/static/media/PlataformasGame5.3e876b57.gif"
+  },
+  {
+    "revision": "67fbb11a6271ac2bb78513110934bc60",
+    "url": "/Skooldy/static/media/PlataformasGame6.67fbb11a.gif"
+  },
+  {
+    "revision": "aae2733b6fce6ae2b316335cf9a86ad4",
+    "url": "/Skooldy/static/media/PlataformasGame7.aae2733b.gif"
+  },
+  {
+    "revision": "c83c687e8f2db7c0b96667ce389b6100",
+    "url": "/Skooldy/static/media/Player.c83c687e.svg"
+  },
+  {
+    "revision": "73002a5c3b38348ab83f493a7d7b8dfd",
+    "url": "/Skooldy/static/media/Profile.73002a5c.svg"
+  },
+  {
+    "revision": "4586a9af52000295e12a77032080827b",
+    "url": "/Skooldy/static/media/Prueba.4586a9af.svg"
+  },
+  {
+    "revision": "adc79d81fb59f538a8162d69b13af04b",
+    "url": "/Skooldy/static/media/Push.adc79d81.flac"
+  },
+  {
+    "revision": "7fc8b9bd774d4775503dacf5d86be47f",
+    "url": "/Skooldy/static/media/Ranking-titulo.7fc8b9bd.svg"
+  },
+  {
+    "revision": "fbd9222fc377a1f892b09d67b7a9b765",
+    "url": "/Skooldy/static/media/Section1.fbd9222f.svg"
+  },
+  {
+    "revision": "4a8c028aba485894a1268c01589ee9d2",
+    "url": "/Skooldy/static/media/Speaker.4a8c028a.svg"
+  },
+  {
+    "revision": "34406c89c7a165b78e43e26e829fb883",
+    "url": "/Skooldy/static/media/Walk1.34406c89.png"
+  },
+  {
+    "revision": "c029e22e515d84505711a5a63b28889d",
+    "url": "/Skooldy/static/media/Walk10.c029e22e.png"
+  },
+  {
+    "revision": "09734d92a6e3972fd37f39f5def0e084",
+    "url": "/Skooldy/static/media/Walk2.09734d92.png"
+  },
+  {
+    "revision": "2cc21a8c9ea0b4c7ad8461cab71a54cf",
+    "url": "/Skooldy/static/media/Walk3.2cc21a8c.png"
+  },
+  {
+    "revision": "79a1d99f9b78d9c39925eb1c9f7f3e15",
+    "url": "/Skooldy/static/media/Walk4.79a1d99f.png"
+  },
+  {
+    "revision": "c2d98d0d5a3a96e418d09f7e9ce65902",
+    "url": "/Skooldy/static/media/Walk5.c2d98d0d.png"
+  },
+  {
+    "revision": "5627e4bf863dbd82c5c045b8721596f6",
+    "url": "/Skooldy/static/media/Walk6.5627e4bf.png"
+  },
+  {
+    "revision": "1522d1972eae100d822be00a13af5f0c",
+    "url": "/Skooldy/static/media/Walk7.1522d197.png"
+  },
+  {
+    "revision": "2828455f73303c1a9f722d005c21c5f5",
+    "url": "/Skooldy/static/media/Walk8.2828455f.png"
+  },
+  {
+    "revision": "628ead3aa8f4040118c3767a86764cca",
+    "url": "/Skooldy/static/media/Walk9.628ead3a.png"
+  },
+  {
+    "revision": "0948f434948f47e56547fae142ac6bd2",
+    "url": "/Skooldy/static/media/button_resp.0948f434.svg"
+  },
+  {
+    "revision": "1b2d10489513ad536d17997d554f54aa",
+    "url": "/Skooldy/static/media/checkbox-check.1b2d1048.svg"
+  },
+  {
+    "revision": "c14ec19542d6e1f0ae42c4c83a0eaae1",
+    "url": "/Skooldy/static/media/checkbox-uncheck.c14ec195.svg"
+  },
+  {
+    "revision": "5397dbd30c1be45f29e5f4c4ea787103",
+    "url": "/Skooldy/static/media/control.5397dbd3.svg"
+  },
+  {
+    "revision": "d984194a68a67c366ea4e2a8f450c381",
+    "url": "/Skooldy/static/media/fondoFooter.d984194a.svg"
+  },
+  {
+    "revision": "9f5fed2d5b83826a4a8ee99923430584",
+    "url": "/Skooldy/static/media/fondoPC1.9f5fed2d.svg"
+  },
+  {
+    "revision": "dffd719f73e53e5750298187d2ad14b6",
+    "url": "/Skooldy/static/media/fondoPC2.dffd719f.svg"
+  },
+  {
+    "revision": "c788e4ab0b17aa32d229ae05575fd539",
+    "url": "/Skooldy/static/media/fondo_Num.c788e4ab.svg"
+  },
+  {
+    "revision": "075b13fdbed961ba8898e7b2c39b715a",
+    "url": "/Skooldy/static/media/fondo_Pc.075b13fd.svg"
+  },
+  {
+    "revision": "ee4ec5ad4197d8ae685ad70fb9d6d17d",
+    "url": "/Skooldy/static/media/footer.ee4ec5ad.svg"
+  },
+  {
+    "revision": "6209ba77cbe9380912dea20f78702fca",
+    "url": "/Skooldy/static/media/header_form.6209ba77.svg"
+  },
+  {
+    "revision": "d98b50ffaf9846fbcba7dcef7a405b6c",
+    "url": "/Skooldy/static/media/iconAutor.d98b50ff.svg"
+  },
+  {
+    "revision": "a108f0b6a1c3d10c0b8290ef81eebbe2",
+    "url": "/Skooldy/static/media/iconBuscar.a108f0b6.svg"
+  },
+  {
+    "revision": "0307be089e21c8aebf9cf3b19dd2b1cf",
+    "url": "/Skooldy/static/media/iconCaif.0307be08.svg"
+  },
+  {
+    "revision": "c54b549b81a6cc9f500a1b46f0b8f2f0",
+    "url": "/Skooldy/static/media/iconCalif.c54b549b.svg"
+  },
+  {
+    "revision": "5da34c6f8624422f8633f779ddcefe3b",
+    "url": "/Skooldy/static/media/iconClases.5da34c6f.svg"
+  },
+  {
+    "revision": "e588bc7db62549e08a885bfc2b00398b",
+    "url": "/Skooldy/static/media/iconClases.e588bc7d.svg"
+  },
+  {
+    "revision": "76b3030db6495c4f7c3d6d2ab78797f4",
+    "url": "/Skooldy/static/media/iconCorreo.76b3030d.svg"
+  },
+  {
+    "revision": "8043c4fdb4c3f53a26229184237377aa",
+    "url": "/Skooldy/static/media/iconEquis.8043c4fd.svg"
+  },
+  {
+    "revision": "15ce5d94857db8b4532a837b924e9eed",
+    "url": "/Skooldy/static/media/iconFacebook.15ce5d94.svg"
+  },
+  {
+    "revision": "1ad9990fe81c67356a34edfd467c0c68",
+    "url": "/Skooldy/static/media/iconFecha.1ad9990f.svg"
+  },
+  {
+    "revision": "1507368b1136f7c6a5cacfaa1d0538be",
+    "url": "/Skooldy/static/media/iconInscrip.1507368b.svg"
+  },
+  {
+    "revision": "87e64460d198711f1f8a15bf772849e5",
+    "url": "/Skooldy/static/media/iconItem.87e64460.svg"
+  },
+  {
+    "revision": "799517ce4b1f42db42706463ac236155",
+    "url": "/Skooldy/static/media/iconJugar.799517ce.svg"
+  },
+  {
+    "revision": "f60aac3aab94010c057dba8c6181fa76",
+    "url": "/Skooldy/static/media/iconJugar.f60aac3a.svg"
+  },
+  {
+    "revision": "bbdff7fed6f1203d170a8e9be88a9e62",
+    "url": "/Skooldy/static/media/iconLapiz.bbdff7fe.svg"
+  },
+  {
+    "revision": "a5a1932e036df910d18a70b69f517777",
+    "url": "/Skooldy/static/media/iconLupa.a5a1932e.svg"
+  },
+  {
+    "revision": "fc85da1aef2c9afebfce5585a80c9cda",
+    "url": "/Skooldy/static/media/iconLupa.fc85da1a.svg"
+  },
+  {
+    "revision": "660c5db3e241941eeaf6ef09065619ac",
+    "url": "/Skooldy/static/media/iconLupa2.660c5db3.svg"
+  },
+  {
+    "revision": "43423abdc71c0b2c5ca49eb50fbd2529",
+    "url": "/Skooldy/static/media/iconNacion.43423abd.svg"
+  },
+  {
+    "revision": "d4b67621828dd8536ed648430f984499",
+    "url": "/Skooldy/static/media/iconNewClas.d4b67621.svg"
+  },
+  {
+    "revision": "e493cf5e5195d514ab8bb5ebd3a9ab0e",
+    "url": "/Skooldy/static/media/iconPerfil.e493cf5e.svg"
+  },
+  {
+    "revision": "b98e32ed6a546022e8cb8e4aa79b248b",
+    "url": "/Skooldy/static/media/iconRanking.b98e32ed.svg"
+  },
+  {
+    "revision": "14113aee9f8534dd627a8d74ef0a121f",
+    "url": "/Skooldy/static/media/iconYoutube.14113aee.svg"
+  },
+  {
+    "revision": "034c882438265d0c2be52b4bb6ba825a",
+    "url": "/Skooldy/static/media/icon_Alien.034c8824.svg"
+  },
+  {
+    "revision": "0bda8bab9714eb116bfcae8bca1e061f",
+    "url": "/Skooldy/static/media/icon_Conj.0bda8bab.svg"
+  },
+  {
+    "revision": "13d009df7824545a5d8dde8774f48926",
+    "url": "/Skooldy/static/media/icon_Pacman.13d009df.svg"
+  },
+  {
+    "revision": "f770b1d7882fb0bb3a6f921dfdea1f54",
+    "url": "/Skooldy/static/media/icon_Pacman.f770b1d7.svg"
+  },
+  {
+    "revision": "a5b7019f6762aa371f37b3967a66c02f",
+    "url": "/Skooldy/static/media/icon_Plat.a5b7019f.svg"
+  },
+  {
+    "revision": "f817d94948d12a5233b3e43e8914ee46",
+    "url": "/Skooldy/static/media/icon_Plat.f817d949.svg"
+  },
+  {
+    "revision": "5601a98ebfc34d69f923f6ce80cd7ac6",
+    "url": "/Skooldy/static/media/icon_Sopa.5601a98e.svg"
+  },
+  {
+    "revision": "6b82cb6aa079a4d8d07d86bf8594cc2c",
+    "url": "/Skooldy/static/media/icon_Sopa.6b82cb6a.svg"
+  },
+  {
+    "revision": "00aa7eb211b08ea49452a376648a2e06",
+    "url": "/Skooldy/static/media/icon_numeros.00aa7eb2.svg"
+  },
+  {
+    "revision": "8f8975e6c894b6f88f4116e993659929",
+    "url": "/Skooldy/static/media/iconoVer.8f8975e6.svg"
+  },
+  {
+    "revision": "19ca8368bc2d6f56a358843a70c000f5",
+    "url": "/Skooldy/static/media/info.19ca8368.svg"
+  },
+  {
+    "revision": "fbcb5b9a721f0eecb3c49a09d3762a7d",
+    "url": "/Skooldy/static/media/layout_activ.fbcb5b9a.svg"
+  },
+  {
+    "revision": "56d60ce133ca5d6be70b81ec250baaa5",
+    "url": "/Skooldy/static/media/menu-titulo.56d60ce1.svg"
+  },
+  {
+    "revision": "716c314358c758ed9882975419c4ae26",
+    "url": "/Skooldy/static/media/movil_fondoDER.716c3143.svg"
+  },
+  {
+    "revision": "162d9cdc3cf337c3b02e95d5b96616b7",
+    "url": "/Skooldy/static/media/movil_fondoIZQ.162d9cdc.svg"
+  },
+  {
+    "revision": "8a1871a27043c0fe66c0db8496e85d74",
+    "url": "/Skooldy/static/media/new_icon.8a1871a2.svg"
+  },
+  {
+    "revision": "979150710fd2d5c450158c1fe743ae22",
+    "url": "/Skooldy/static/media/ojo.97915071.svg"
+  },
+  {
+    "revision": "b491cbe7559f5677c840b7283ad4e8f5",
+    "url": "/Skooldy/static/media/plat_fondo.b491cbe7.svg"
+  },
+  {
+    "revision": "f0538c3ec6697003f883ed3c815ce57f",
+    "url": "/Skooldy/static/media/search-titulo.f0538c3e.svg"
+  },
+  {
+    "revision": "2a554ee0e298c01abad6df9bb84e1641",
+    "url": "/Skooldy/static/media/titulo-clases.2a554ee0.svg"
+  },
+  {
+    "revision": "9125417636434fff85ecb695caa2d525",
+    "url": "/Skooldy/static/media/tituloClases.91254176.svg"
+  },
+  {
+    "revision": "63a64f277f7e9db095d61f0a85d0723e",
+    "url": "/Skooldy/static/media/tituloNewClase.63a64f27.svg"
+  },
+  {
+    "revision": "ea5d1dab5827f2ab4d37e928405feac2",
+    "url": "/Skooldy/static/media/titulo_calif.ea5d1dab.svg"
+  },
+  {
+    "revision": "3b6a91ca4802e6bb8f36e2bb0bde719f",
+    "url": "/Skooldy/static/media/titulo_jugar.3b6a91ca.svg"
+  },
+  {
+    "revision": "47e2682b9bff848c813779efe73a2469",
+    "url": "/Skooldy/static/media/titulo_newAct.47e2682b.svg"
+  },
+  {
+    "revision": "b2cbf2ae83d83605c960139e66243185",
+    "url": "/Skooldy/static/media/vidas_icon.b2cbf2ae.svg"
+  },
+  {
+    "revision": "78033e96dacef3173509779c43060f20",
+    "url": "/Skooldy/static/media/yyy.78033e96.png"
+  }
+]);
